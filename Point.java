beaff9746345a818be7a5a55f54985a5bbb25023
Aditi Haiman <ahaiman00@stuy.edu>
2018-10-10 12:59:31 -0400

    added files

---
+++ Point.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+
+public class Point{
+  private double x, y;
+
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+
+  public double distanceTo(Point a) {
+    return Math.sqrt(Math.pow(this.getX() + a.getX(), 2)+Math.pow(this.getY() + a.getY(), 2));
+  }
+
+  public static double distance(Point a, Point b) {
+    return Math.sqrt(Math.pow(b.getX() + a.getX(), 2)+Math.pow(b.getY() + a.getY(), 2));
+  }
+}
---
+++ Triangle.java
@@ -0,0 +1,41 @@
+public class Triangle {
+  private Point a, b, c;
+
+  public Triangle(Point A, Point B, Point C) {
+    a = new Point(A.getX(), A.getY());
+    b = new Point(B.getX(), B.getY());
+    c = new Point(C.getX(), C.getY());
+  }
+
+  public void setA(Point A) {
+    a = new Point(A.getX(), A.getY());
+  }
+
+  public void setB(Point B) {
+    b = new Point(B.getX(), B.getY());
+  }
+
+  public void setC(Point C) {
+    c = new Point(C.getX(), C.getY());
+  }
+
+  public Point getA() {
+    return a;
+  }
+
+  public Point getB() {
+    return b;
+  }
+
+  public Point getC() {
+    return c;
+  }
+
+  public double perimeter(Triangle tri) {
+    Point x = tri.getA();
+    Point y = tri.getB();
+    Point z = tri.getC();
+    return x.distanceTo(y) + y.distanceTo(z) + z.distanceTo(x);
+  }
+
+}
